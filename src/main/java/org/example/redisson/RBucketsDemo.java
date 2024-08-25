6114b8d7fb42b58e80d201836a81a85670ec00fb
jsszwc <jsszwc1st@gmail.com>
2024-08-25 14:12:49 +0800

    redisson之RBuckets描述纠错

--- src/main/java/org/example/redisson/RBucketsDemo.java
+++ src/main/java/org/example/redisson/RBucketsDemo.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 /**
  * 允许用户同时操作多个Redis中的RBucket，使用RBuckets可以执行批量的读取、写入、更新或删除操作，这些操作通常比单独对每个键进行操作更高效
- * 批量操作： RBuckets通过Redis的管道（pipelining）功能，将多个命令打包在一起用一次网络请求发送到 Redis 服务器。这样可以减少客户端和服务器之间的往返时间（RTT）
+ * 批量操作： RBuckets本身是利用MGET、MSET等命令批量操作的，这样可以减少客户端和服务器之间的往返时间（RTT）
  * 原子性： 尽管 RBuckets 提供了批量操作的便利，但批量操作本身并不是原子性的。如果需要原子性操作，可以使用 Lua 脚本或 Redis 事务（使用 MULTI/EXEC 命令）。
  */
 public class RBucketsDemo {
